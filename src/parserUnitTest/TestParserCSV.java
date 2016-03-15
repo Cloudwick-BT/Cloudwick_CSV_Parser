14735b0ad467e11701146a85feba1fbb163b9d28
bhavintandel <bhautandel@yahoo.com.com>
2016-03-15 11:12:17 +0000

    added test unit to check avilability of file

--- src/parserUnitTest/TestParserCSV.java
+++ src/parserUnitTest/TestParserCSV.java
@@ -1,5 +1,24 @@
 package parserUnitTest;
 
+import java.io.Reader;
+import libraryparser.*;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * 
+ * @author Bhavin
+ * @since 15/03/2016; 11:00
+ */
+
 public class TestParserCSV {
+	
+	@Test
+	public void testInput(){
+		ParsingCSV parser = new ParsingCSV();
+		String fileName = "F:\\Cloudwick\\java_Cloudwick\\CSVParser\\data.csv";
+		Reader test = parser.getFileReader(fileName);
+		assertNotNull(test);
+	}
 
 }
